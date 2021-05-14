d8d15ba9944b2fe300f0b67c146185975fbe8b22
Davk01 <davitwjob@gmail.com>
2021-05-14 11:46:21 +0400

    Create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
@@ -8,15 +8,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  /*
-  hero = 'Windstorm';
-  */
-  heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  @Input() heroes = HEROES;
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
